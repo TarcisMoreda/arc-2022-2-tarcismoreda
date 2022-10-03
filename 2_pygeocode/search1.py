77f4f552e1fff865fef73d6d5b6078a98a812feb
TarcisMoreda <tarcismoreda@gmail.com>
2022-10-03 16:00:57 -0300

    search1 2.1

--- 2_pygeocode/search1.py
+++ 2_pygeocode/search1.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
 	location = Nominatim(user_agent=user_agent).geocode(address, exactly_one=False)
 	print('Endereço buscado: {0}\n'.format(address))
 	for i in range(len(location)):
-		print('\tResultado {0}:\n'.format(str(i)))
+		print('\tResultado {0}:\n'.format(str(i+1)))
 		print('\t\tCEP: {0}\n'.format(location[i].address.split(', ')[-2]))
 		print('\t\t(Latitude, Longitude): ({0}, {1})\n'.format(str(location[i].latitude), str(location[i].longitude)))
