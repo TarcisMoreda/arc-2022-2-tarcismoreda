5abb7f5071096ca02a7659209d99fb6195ecb15e
TarcisMoreda <tarcismoreda@gmail.com>
2022-10-04 07:25:49 -0300

    search2 2.0

--- 2_pygeocode/search2.py
+++ 2_pygeocode/search2.py
@@ -2,14 +2,19 @@
 import requests
 
 def geocode(address):
-    base = 'https://nominatim.openstreetmap.org/search'
-    parameters = {'q': address, 'format': 'json'}
-    user_agent = 'Search2.py'
-    headers = {'User-Agent': user_agent}
-    response = requests.get(base, params=parameters, headers=headers)
-    reply = response.json()
-    print(reply[0]['lat'], reply[0]['lon'])
+	base = 'https://nominatim.openstreetmap.org/search'
+	parameters = {'q': address, 'format': 'json'}
+	user_agent = 'Search2.py'
+	headers = {'User-Agent': user_agent}
+	response = requests.get(base, params=parameters, headers=headers)
+	return response.json()
 
 
 if __name__ == '__main__':
-    geocode('Belarmino Vilela Junqueira, Ituiutaba, MG')
+	address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
+	reply = geocode(address)
+	print('Endereço buscado: {0}\n'.format(address))
+	for i in range(len(reply)):
+		print('\tResultado {0}:\n'.format(str(i+1)))
+		print('\t\tCEP: {0}\n'.format(reply[i]['display_name'].split(', ')[-2]))
+		print('\t\t(Latitude, Longitude): ({0}, {1})\n'.format(str(reply[i]['lat']), str(reply[i]['lon'])))
