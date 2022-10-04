88b9700c002d4acb75d14eadac374e534cbf7dfe
TarcisMoreda <tarcismoreda@gmail.com>
2022-10-04 07:55:47 -0300

    search4 2.0

--- 2_pygeocode/search4.py
+++ 2_pygeocode/search4.py
@@ -2,6 +2,7 @@
 import socket
 import ssl
 from urllib.parse import quote_plus
+import json
 
 request_text = """\
 GET /search?q={}&format=json HTTP/1.1\r\n\
@@ -13,19 +14,26 @@ Connection: close\r\n\
 
 
 def geocode(address):
-    unencrypted_sock = socket.socket()
-    unencrypted_sock.connect(('nominatim.openstreetmap.org', 443))
-    sock = ssl.wrap_socket(unencrypted_sock)
-    request = request_text.format(quote_plus(address))
-    sock.sendall(request.encode('ascii'))
-    raw_reply = b''
-    while True:
-        more = sock.recv(4096)
-        if not more:
-            break
-        raw_reply += more
-    print(raw_reply.decode('utf-8'))
-
+	unencrypted_sock = socket.socket()
+	unencrypted_sock.connect(('nominatim.openstreetmap.org', 443))
+	sock = ssl.wrap_socket(unencrypted_sock)
+	request = request_text.format(quote_plus(address))
+	sock.sendall(request.encode('ascii'))
+	raw_reply = b''
+	while True:
+		more = sock.recv(4096)
+		if not more:
+			break
+		raw_reply += more
+	reply = raw_reply.decode('utf-8')
+	reply = reply[reply.find('['):]
+	return json.loads(reply[:reply.find(reply[-1])])
 
 if __name__ == '__main__':
-    geocode('Belarmino Vilela Junqueira, Ituiutaba, MG')
+	address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
+	reply = geocode(address)
+	print('Endereço buscado: {0}\n'.format(address))
+	for i in range(len(reply)):
+		print('\tResultado {0}:\n'.format(str(i+1)))
+		print('\t\tCEP: {0}\n'.format(reply[i]['display_name'].split(', ')[-2]))
+		print('\t\t(Latitude, Longitude): ({0}, {1})\n'.format(str(reply[i]['lat']), str(reply[i]['lon'])))
